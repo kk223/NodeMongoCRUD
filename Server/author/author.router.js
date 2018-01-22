0cb222d7c86cab8741ae850aba1a7055c04146f0
kk223 <kaushaljha48@gmail.com>
2018-01-23 00:53:46 +0530

    route for book and author added

--- Server/author/author.router.js
+++ Server/author/author.router.js
@@ -1,22 +1,35 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('./users.controller');
+const express = require('express');
+const router = express.Router();
+const authorController = require('./author.controller');
+const user = require('./auhtor.entity');
+/*
+ * What this API do: It'll pass the user detail and fetch the database in order to decide whether the user exist or not and
+ *                   on the basis of that preform the desired action
+ */
+router.get('/:authorname', function(req, res) {
+    try {
+        let authorname = req.params.authorname;
+        authorController.findAuthor(authorame).then(function(user) {
+            res.status(200).send({user: user});
+        }, function(err) {
+            res.status(500).send({error: 'Failed to complete operation...!'});
+        });
+    } catch (ex) {
+        res.status(500).send({error: 'Internal server error.'});
+    }
+});
 
-// /*
-//  * What this API do: It'll pass the user detail and fetch the database in order to decide whether the user exist or not and
-//  *                   on the basis of that preform the desired action
-//  */
-// router.get('/:username', function(req, res) {
-//     try {
-//         let username = req.params.username;
-//         userController.findUser(username).then(function(user) {
-//             res.status(200).send({user: user});
-//         }, function(err) {
-//             res.status(500).send({error: 'Failed to complete operation...!'});
-//         });
-//     } catch (ex) {
-//         res.status(500).send({error: 'Internal server error.'});
-//     }
-// });
+router.post('/:createauthor',function(req,res){
+    try{
+                let author= new author(req.body);
+                authorController.createAuthor(author).then(function(user){
+                    res.status(200).send({user:user.name  "created"});
+                },function(err){
+                    res.status(500).send({error: "failed to complete operation....!"});
+                });
+    } catch(ex){
+        res.status(500).send({error: 'Internal error'});
+    }
+})
 
-// module.exports = router;
+module.exports = router;
--- Server/books/books.router.js
+++ Server/books/books.router.js
@@ -1,22 +1,40 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('./users.controller');
+const express = require('express');
+const router = express.Router();
+const booksController = require('./books.controller');
+const user = require('./books.entity');
+/*
+ * What this API do: It'll pass the user detail and fetch the database in order to decide whether the user exist or not and
+ *                   on the basis of that preform the desired action
+ */
+router.get('/:bookname', function(req, res) {
+    try {
+        let bookname = req.params.bookname;
+        booksController .findUser(bookname).then(function(user) {
+            res.status(200).send({user: user});
+        }, function(err) {
+            res.status(500).send({error: 'Failed to complete operation...!'});
+        });
+    } catch (ex) {
+        res.status(500).send({error: 'Internal server error.'});
+    }
+});
 
-// /*
-//  * What this API do: It'll pass the user detail and fetch the database in order to decide whether the user exist or not and
-//  *                   on the basis of that preform the desired action
-//  */
-// router.get('/:username', function(req, res) {
-//     try {
-//         let username = req.params.username;
-//         userController.findUser(username).then(function(user) {
-//             res.status(200).send({user: user});
-//         }, function(err) {
-//             res.status(500).send({error: 'Failed to complete operation...!'});
-//         });
-//     } catch (ex) {
-//         res.status(500).send({error: 'Internal server error.'});
-//     }
-// });
+router.post('/:booksprofile',function(req.res){
+    try{
+        let 
+    }
+})
+router.post('/:createbooks',function(req,res){
+    try{
+                let books= new books(req.body);
+               booksController .createbooks(books).then(function(user){
+                    res.status(200).send({books:books.name  "created"});
+                },function(err){
+                    res.status(500).send({error: "failed to complete operation....!"});
+                });
+    } catch(ex){
+        res.status(500).send({error: 'Internal error'});
+    }
+})
 
-// module.exports = router;
+module.exports = router;
